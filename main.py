7f9aa99f4dbabd29460306497cdb3634395e3056
Akazukin8763 <starmoon900709@gmail.com>
2024-06-17 16:39:47 +0800

    feat: 更新程式進入點
    1. 將 object_remover.py 的程式進入點改至 main.py

--- main.py
+++ main.py
@@ -1,49 +1,16 @@
-import cv2
-import pafy
-
-from models import YOLOv8Seg
-from capture import MediaCapture
+from object_remover import ObjectRemover
 
 
 def main():
-    model = YOLOv8Seg()
-    model.load_model(gpu=True)
+    # filepath = './resources/IMG_1722.jpg'
+    filepath = './resources/640x360_Zebra.mp4'
+    # filepath = './resources/640x360_Birds.mp4.mp4'
+    # filepath = './resources/640x360_Eagle.mp4'
+    # filepath = './resources/640x360_Giraffe.mp4'
 
-    url = 'https://www.youtube.com/watch?v=L4U4woKIC_w&list=PLWLEgnHzuoYx8Y4s_HPV1H1EYmLT6X74E&index=85'
-    # url = 'https://www.youtube.com/watch?v=gFRtAAmiFbE&list=PL-Ni-1OtjEdLtQRpD-6r9AsD3P_6MLpgv&index=96'
-    video = pafy.new(url)
-    best = video.getbest(preftype="mp4")
-
-    capture = MediaCapture(best.url, onstream=True)
-    win_width = int(capture.width // 2)
-    win_height = int(capture.height // 2)
-
-    while True:
-        frame = capture.read()
-
-        if frame is None:
-            continue
-
-        model.predict(frame)
-        output_image = model.draw_detections(
-            model.predict_image,
-            model.result_boxes,
-            model.result_classes,
-            model.result_confs,
-            model.result_masks
-        )
-
-        win_name = 'Output'
-        cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(win_name, win_width, win_height)
-        cv2.imshow(win_name, output_image)
-
-        # Close the window (ESC)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-
-    capture.release()
-    cv2.destroyAllWindows()
+    remover = ObjectRemover()
+    remover.load(filepath)
+    remover.run(learning_base=False)
 
 
 if __name__ == '__main__':
--- object_remover.py
+++ object_remover.py
@@ -519,19 +519,3 @@ class ObjectRemover():
             results.extend(self.model_propainter.predict(frame_chunk, mask_chunk))
 
         return results
-
-
-def main():
-    # filepath = './resources/IMG_1722.jpg'
-    filepath = './resources/640x360_Zebra.mp4'
-    # filepath = './resources/640x360_Birds.mp4.mp4'
-    # filepath = './resources/640x360_Eagle.mp4'
-    # filepath = './resources/640x360_Giraffe.mp4'
-
-    remover = ObjectRemover()
-    remover.load(filepath)
-    remover.run(learning_base=False)
-
-
-if __name__ == '__main__':
-    main()
